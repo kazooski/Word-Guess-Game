0ac4a0b5773021d4d165d36665acc0490f0c5373
noisette <kasusky.music@gmail.com>
2020-04-23 00:02:49 -0700

    JS: array of items added

---
+++ assets/javascript/game.js
@@ -0,0 +1,40 @@
+// Javascript for game
+// The splicing of a pantrySelection
+        // while( array.length ) {
+        //     var index = Math.floor( Math.random()*array.length );
+        //     console.log( array[index] ); // Log the item
+        //     array.splice( index, 1 ); // Remove the item from the array
+        // }
+
+// Global variables
+
+var pantryItems = [
+    {food: "olive oil", image: ""},
+    {food: "vegetable oil", image: ""},
+    {food: "balsamic vinegar", image: ""},
+    {food: "red wine vinegar", image: ""},
+    {food: "kosher salt", image: ""},
+    {food: "pepper", image: ""},
+    {food: "canned tuna", image: ""},
+    {food: "beans", image: ""},
+    {food: "rice", image: ""},
+    {food: "quinoa", image: ""},
+    {food: "pasta", image: ""},
+    {food: "flour", image: ""},
+    {food: "granulated sugar", image: ""},
+    {food: "garlic", image: ""},
+    {food: "onions", image: ""},
+    {food: "butter", image: ""},
+    {food: "milk", image: ""},
+    {food: "eggs", image: ""},
+    {food: "carrot", image: ""},
+    {food: "mustard", image: ""},
+    {food: "mayonnaise", image: ""},
+    {food: "lemon", image: ""},
+    {food: "nuts", image: ""},
+    {food: "bread", image: ""},
+    {food: "potato", image: ""},
+    {food: "thyme", image: ""},
+    {food: "bell pepper", image: ""},
+]
+
